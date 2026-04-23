121b2c5271d8e42322f98356fcd067f4d8bcd64f
Илья Ватютов <vatut007@ya.ru>
2026-04-23 16:24:53 +0300

    fix

--- src/api/endpoints/payment.py
+++ src/api/endpoints/payment.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException, Header
+from sqlalchemy.exc import IntegrityError
 
 from services.payment import PaymentService
-from shemas.payment import PaymentPublic
+from shemas.payment import PaymentCreate, PaymentPublic
+from models import Payment
 
 router = APIRouter()
 
@@ -10,10 +15,36 @@ router = APIRouter()
     "/payments",
     description="Создание платежа",
     name="Создание платежа",
-    response_model=PaymentPublic
+    response_model=PaymentPublic,
+    status_code=HTTPStatus.ACCEPTED
 )
 async def create_payment(
+    payment: PaymentCreate,
+    idempotency_key: Annotated[str, Header(alias="Idempotency-Key")],
     paymentService: PaymentService = Depends()
 ):
-    payment_public = await paymentService.create_paymnet()
+    try:
+        payment_public = await paymentService.create_payment(
+            payment, idempotency_key)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail=f"Платёж с idempotency_key={idempotency_key} уже существует"
+        )
     return payment_public
+
+
+@router.get(
+    "/payments/{id}",
+    description="Получение платежа",
+    name="Получения платежа",
+    response_model=Payment
+)
+async def get_router(id: int, paymentService: PaymentService = Depends()):
+    payment = await paymentService.get_payment(id)
+    if payment is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail="Не найден платеж с таким идентификатором"
+        )
+    return payment
--- src/services/payment.py
+++ src/services/payment.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 
-from fastapi import Depends, Header
+from fastapi import Depends, HTTPException, Header
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
 
 from broker.rabbit import broker
 from db.db import get_async_session
@@ -11,22 +13,24 @@ from shemas.payment import PaymentCreate, PaymentPublic
 
 class PaymentService:
     def __init__(self,
-                 payment: PaymentCreate,
-                 idempotency_key: Annotated[str,
-                                            Header(alias="Idempotency-Key")],
                  session: AsyncSession = Depends(get_async_session)
                  ) -> None:
-        self.paymentForm = payment
-        self.idempotency_key = idempotency_key
         self.session = session
 
-    async def create_paymnet(self) -> PaymentPublic:
-        payment = Payment(**self.paymentForm.model_dump(),
-                          idempotency_key=self.idempotency_key)
+    async def create_payment(self, paymentForm: PaymentCreate,
+                             idempotency_key: str) -> PaymentPublic:
+        payment = Payment(**paymentForm.model_dump(),
+                          idempotency_key=idempotency_key)
         payment.webhook_url = str(payment.webhook_url)
         self.session.add(payment)
         await self.session.commit()
         await self.session.refresh(payment)
         await broker.publish(payment.id, queue="payment.new")
         return PaymentPublic(**payment.model_dump(
-            include={"id", "status", "created_at"}))
+                include={"id", "status", "created_at"}))
+
+    async def get_payment(self, id: int) -> Payment | None:
+        payment = await self.session.execute(
+            select(Payment).where(Payment.id == id))
+        payment = payment.scalars().first()
+        return payment
